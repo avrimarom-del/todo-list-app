72664a02f346e14bc006b57cef7fa8b2aa793fca
Avri Marom <avrimarom@gmail.com>
2025-09-20 16:56:56 +0300

    handling form inputs

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import Tag from './Tag';
 import './TaskForm.css';
-import './Tag.css';
+import "./Tag"
+
 const TaskForm = () => {
+    const[taskData, setTaskData] = useState({
+        task: "",
+        status: "todo"
+    })
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+
+        setTaskData(prev => {
+            return {...prev,[name]: value}
+        })
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(taskData);
+    }
+
+    /*
+    const [task, setTask] = useState('');
+    const [status, setStatus] = useState('todo')
+    
+    const handleTaskChange = (e) => {
+        setTask(e.target.value);
+    }
+
+    const handleStatusChange = (e) => {
+        setStatus(e.target.value);
+        }*/
+
     return (
         <header className='app_header'>
-            <form>
-                <input type="text" className='task_input' placeholder='Enter your task' />
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="text"
+                name='task' 
+                className='task_input' 
+                placeholder='Enter your task' 
+                onChange={handleChange} 
+                />
+
                 <div className='task_form_bottom_line'>
                     <div>
                         <Tag tagName="HTML" />
@@ -15,7 +54,7 @@ const TaskForm = () => {
                         <Tag tagName="React" />
                     </div>
                     <div>
-                        <select className='task_status'>
+                        <select className='task_status' onChange={handleChange} name="status" id="status">
                             <option value="todo">To Do</option>
                             <option value="doing">Doing</option>
                             <option value="done">Done</option>
