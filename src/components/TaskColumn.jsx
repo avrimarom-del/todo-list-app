7ae247b81143f96d168ca674733c57ea785a9e4c
Avri Marom <avrimarom@gmail.com>
2025-09-21 10:39:06 +0300

    creating drop area component

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import TaskCard from "./TaskCard";
 import "./TaskColumn.css"
+import DropArea from "./DropArea";
 
-const TaskColumn = ({icon, title, tasks, status, handleDelete}) => {
+const TaskColumn = ({icon, title, tasks, status, handleDelete, setActiveCard}) => {
     return (
         <section className="task_column">
             <h2 className="task_column_heading">
@@ -11,13 +12,16 @@ const TaskColumn = ({icon, title, tasks, status, handleDelete}) => {
             
             {
                 tasks.map((task, index) => task.status === status && ( 
+                <React.Fragment key={index}>
                 <TaskCard
-                    key={index}
                     title={task.task}
                     tags={task.tags} 
                     handleDelete={handleDelete}
                     index={index}
+                    setActiveCard={setActiveCard}
                     />
+                    <DropArea />
+                    </React.Fragment>
                 )
             )}
         </section>
